195169be0f3c9b22b0f723c284d946bd47824ffa
Osbaldo Gonzalez Jr <osbaldogonzalezislc@gmail.com>
2021-03-27 09:47:48 -0700

    Autocommit at Sat Mar 27 09:47:48 PDT 2021

--- sieve_eratos.c
+++ sieve_eratos.c
@@ -10,62 +10,55 @@ if a bit is == 1, then its prime, otherwise
 if bit is == 0 it is NOT prime.
 We will be counting from least sig bit*/
 
-struct sieve{
-	uint64_t maxnumber; //maxnumber to check as prime
-	uint64_t *sievearray;
-	uint64_t arraysize;
-	uint64_t checknum;
-	uint64_t arrayindex;
-}
-
-
-int getstatus(struct sieve *soe);
-int setNOTprime(struct sieve *soe);
 
 int main(void)
 {
-	struct sieve soe; //soe == sieve of eratosthenes
-	uint64_t checktill;
-	/*printf("What is the max number you want to check for primes? ");
-	scanf("%lu", &soe.maxnumber); */
-	if (soe.maxnumber <= 1)
+	uint64_t maxnumber;
+	printf("What is the max number you want to check for primes? ");
+	scanf("%lu", &maxnumber); 
+	if (maxnumber <= 1)
 	{
 		printf("Primes are ALWAYS greater than or equal to 2!\n");
 		return 42; //error
 	}
 
-	soe.maxnumber = 32;
-	checktill = sqrt(soe.maxnumber);
-	soe.arraysize = (soe.maxnumber/64) + 1; //64 bits in 8 bytes
-	soe.sievearray = malloc(soe.arraysize*sizeof(uint64_t));
-	soe.checknum = 2; //start with first prime
+	/*If I was under space constraints I could use 32bit var for
+	"checktill", the downside being that maxnumber could not be
+	larger than 2^64 - 1 - 2(2^32 - 1), on side note figured this
+	out by taking max representable number with 64 bits (unsigned
+	of course) and subtracting that by the the max representable
+	number with 32 bits squared aka: 2^64 - 1 - (2^32 - 1)^2 which 
+	equals 2(2^32 - 1). But even though 2(2^32 - 1) is a huge number
+	I could still represent 99.9999999534% of the numbers that 2^64 -1
+	is able to cover. But I'm loaded with RAM so we chillin B) */
+	uint64_t checktill = sqrt(maxnumber); 
+	uint64_t arraysize = (maxnumber/64) + 1; //64 bits in 8 bytes
+	uint64_t sievearray = malloc(arraysize*sizeof(uint64_t));
+	uint64_t checknum = 2; //start with first prime
 	uint64_t currnum; //counter
 
 	//set all bytes to 0xff
-	memset(soe.sievearray, 0xff, soe.arraysize*sizeof(uint64_t));
-	/*start counting by 1, therefore least sig bit in sieve[0] represent # 1
-	and since 1 is NOT prime, set least sigbit of sieve[0] to 0 */
-	soe.sievearray[0] <<= 1;
+	memset(sievearray, 0xff, arraysize*sizeof(uint64_t));
+	/*make bit number 0 and bit number 1 NOT prime by setting them == 0*/
+	sievearray[0] <<= 2;
 
 	//look for primes
 	while(checktill--)
 	{	
-		if (getstatus(sieve, currnumber))
-		{
-			for(currnumber = currnumber; number < maxnumber; number++)
-			{
-			//makeNOTprime
-			}
-		}
-		currnumber++;
+		//check if current "checknum" is prime
+		if ( sievearray[checknum>>6]&(1<<(checknum&63)) ) //acts as bit mask
+			for(currnum = checknum+checknum; currnum < maxnumber; currnum += checknum)
+				sievearray[currnum>>6] &= ~(1<<(currnum&63)); //>>6 == /64 && &63 == %64
+		checknum++;
 	}
 
 	//check number of 1s, inorder to check number of primes
-	free(sieve);
+	uint64_t numberofprimes = 0;
+	for(uint64_t i = 0; i < arraysize; i++)
+		for(;sievearray[i]; sievearray[i] &= sievearray[i] - 1, numberofprimes++);
+
+	printf("Number of primes: %lu\n", numberofprimes);
+	free(sievearray);
 	return 0;
 }
 
-int getstatus(struct sieve *soe)
-{		
---------------------------------------------------------------------------------
-}
