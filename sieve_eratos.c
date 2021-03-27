c551eeec47eab882cfc0c6f8399d199e87edcf27
Osbaldo Gonzalez Jr <osbaldogonzalezislc@gmail.com>
2021-03-27 03:55:25 -0700

    Autocommit at Sat Mar 27 03:55:25 PDT 2021

--- sieve_eratos.c
+++ sieve_eratos.c
@@ -3,13 +3,25 @@
 #include <stdlib.h>
 #include <math.h>
 
-
+/*each "new" bit represents a new number
+therefore 1 byte is a 8 different numbers
+if a bit is == 1, then its prime, otherwise
+if bit is == 0 it is NOT prime.*/
 int main(void)
 {
-	uint64_t numbits;
-	printf("How many bits do you want?");
-	scanf("%lu", &numbits);
-	uint64_t *seive = calloc( (numbits/64) + 1, sizeof(uint64_t) );
+	uint64_t maxnumber;
+	printf("What is the max number you want to check for primes? ?");
+	scanf("%lu", &maxnumber);
 
-	free(seive);
+	uint64_t *sieve = calloc( (maxnumber/64) + 1, sizeof(uint64_t) );
+	uint64_t checktill = sqrt(maxnumber);
+	uint64_t currnum = 3;
+	while(checktill--)
+	{
+		
+	}
+	
+
+	//check number of 1s, inorder to check number of primes
+	free(sieve);
 }
---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	printf("%ld\n", sizeof(~0L));
+	return 0;
+}
